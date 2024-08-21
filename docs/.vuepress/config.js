1ebff23d9207b45a337a7efc4f066065af4f5b16
neverbiasu <1751162157@qq.com>
2024-08-21 23:30:40 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+import { webpackBundler } from '@vuepress/bundler-webpack'
+import { defineUserConfig } from 'vuepress'
+import { defaultTheme } from '@vuepress/theme-default'
+
+export default defineUserConfig({
+  bundler: webpackBundler({
+    postcss: {},
+    vue: {},
+  }),
+  theme: defaultTheme({
+    hostname: 'localhost',
+  }),
+})
