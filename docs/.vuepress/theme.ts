169d78bd2cebc77608471ba7490a39dd1be62dab
neverbiasu <1751162157@qq.com>
2024-08-22 14:54:31 +0800

    features: Init theme

---
+++ docs/.vuepress/theme.ts
@@ -0,0 +1,24 @@
+import { getDirname, path } from 'vuepress/utils';
+import { socialSharePlugin } from 'vuepress-plugin-social-share';
+import { removeHtmlExtensionPlugin } from 'vuepress-plugin-remove-html-extension';
+import { mediumZoomPlugin } from '@vuepress/plugin-medium-zoom';
+
+const __dirname = getDirname(import.meta.url);
+
+const faychTheme = {
+  return: {
+    name: 'faych-theme',
+
+    clientConfigFile: path.resolve(__dirname, 'client.ts'),
+
+    // templateBuild: path.resolve(__dirname, 'templates/build.html'),
+    // templateDev: path.resolve(__dirname, 'templates/dev.html'),
+
+    plugins: [
+      ['@vuepress/plugin-catalog'],
+      [socialSharePlugin()],
+      [mediumZoomPlugin()],
+      [removeHtmlExtensionPlugin()]
+    ]
+  }
+};
